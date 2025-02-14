398398fb870b7cc002a52422f35302ce2adc5c11
Retype15 <reynierramos280@gmail.com>
2025-02-13 23:01:57 -0500

    v0.7.14

---
+++ static_datas.py
@@ -0,0 +1,79 @@
+dict_menu_types = {
+    "alcoholic_drinks": "bebidas alcohólicas",
+    "appetizer": "aperitivo",
+    "breakfast": "desayuno",
+    "cocktails": "cócteles",
+    "desserts": "postres",
+    "infusions": "infusiones",
+    "mains": "platos principales",
+    "non_alcoholic_drinks": "bebidas sin alcohol",
+    "others": "otros",
+    "pasta": "pasta",
+    "pizza": "pizza",
+    "salads": "ensaladas",
+    "sandwich": "sándwich",
+    "seafood": "mariscos",
+    "sides": "guarniciones",
+    "soups": "sopas",
+    "starters": "entrantes",
+    "water": "agua",
+    "wines": "vinos"
+}
+
+dict_menu_data = {
+    'Platos principales': 'menu_mains',
+    'Desayuno': 'menu_breakfast',
+    'Aperitivo': 'menu_appetizer',
+    'Entrantes': 'menu_starters',
+    'Pizza': 'menu_pizza',
+    'Pasta': 'menu_pasta',
+    'Mariscos': 'menu_seafood',
+    'Sándwich': 'menu_sandwich',
+    'Guarniciones': 'menu_sides',
+    'Ensaladas': 'menu_salads',
+    'Sopas': 'menu_soups',
+    'Postres': 'menu_desserts',
+    'Cócteles': 'menu_cocktails',
+    'Vinos': 'menu_wines',
+    'Bebidas alcohólicas': 'menu_alcoholic_drinks',
+    'Bebidas no alcohólicas': 'menu_non_alcoholic_drinks',
+    'Infusiones': 'menu_infusions',
+    'Agua': 'menu_water',
+    'Otros': 'menu_others',
+    'Todos': 'menu'
+}
+
+dict_args = {
+    'Promedio': 'mean',
+    'Mediana': 'median',
+    'Más Caro': 'max',
+    'Más Barato': 'min',
+    'Suma de todo': 'sum'
+}
+
+dict_color_levels = {
+    'Económico': 'MediumBlue',
+    'Medio': 'mediumorange',
+    'Alto': 'SeaGreen',
+    'Lujoso': 'mediumpurple'
+}
+
+dict_data_labels = {
+    "est_type": 'Tipo de establecimiento',
+    "cuisine": 'Tipo de cocina',
+    "phone": 'Telefono',
+    "rating": 'Puntuación',
+    "cap": 'Capacidad',
+    "loc_street": 'Calle',
+    "loc_municipe": 'Municipio',
+    "count": 'Cantidad',
+    "level": 'Nivel de accesibilidad económica',
+    'clasif': 'Clasificación',
+    'reviews': 'Cantidad de reseñas',
+    'method': 'Método de Reserva',
+    'count': 'Cantidad',
+    'menu_types': 'Tipos de menú',
+
+    "loc_x": 'Ubicacion en (X)',
+    "loc_y": 'Ubicacion en (Y)',
+}
